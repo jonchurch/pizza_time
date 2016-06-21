8780cf6a1d8651fb25ae3fc798448996974112fc
Jon Church <me@jonchurch.com>
2016-06-21 19:43:54 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(req, res) {
+    res.sendFile('public/html/index.html', {
+        root: __dirname
+    })
+})
+
+
+
+
+
+app.listen(8080, function() {
+    console.log('Express listening on 8080...');
+})
