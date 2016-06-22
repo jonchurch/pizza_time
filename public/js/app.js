cfdb2b846cadc36cf488f5c7dd5ef4fb3e7c5b99
Jon Church <me@jonchurch.com>
2016-06-22 19:14:51 -0400

    added countdown logic bones

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,60 @@
-$(document).ready(function(){
-	var _start = $('#start');
-	var _break = $('#break');
-	var _minLeft = $('#minutes-left');
-	var _minRight = $('#minutes-right');
-	var _secLeft = $('#seconds-left');
-	var _secRight = $('#seconds-right');
-	var breakCount= 0;
+$(document).ready(function() {
+    var _start = $('#start');
+    var _break = $('#break');
+    var _minLeft = $('#minutes-left');
+    var _minRight = $('#minutes-right');
+    var _secLeft = $('#seconds-left');
+    var _secRight = $('#seconds-right');
+    var breakCount = 0;
 
-	_start.on('click', startCountdown);
-	_break.on('click', breakStart);
-
-
-	function startCountdown(){
-		//start it from 25:00
-		_start.attr('disabled', true);
-		_start.addClass('disabled');
-		//I need to associate a number with these id's somehow..
-		//_.removeClass('em-' + )
+    _start.on('click', startCountdown);
+    _break.on('click', breakStart);
 
 
-	}
+    function startCountdown() {
+        //start it from 25:00
+        _start.attr('disabled', true);
+        _start.addClass('disabled');
+        //I need to associate a number with these id's somehow..
+        //_.removeClass('em-' + )
+        var countinterval = setInterval(function() {
+            var minsNum = 25;
+            var secNum = 0;
 
-	function breakStart(){
-		//start break from 5:00, unless it is the third break
-		_break.attr('disabled', true);
-		_break.addClass('disabled');
+            if (secNum === 0 && minsNum === 0) {
+                breakBtn.removeClass('disabled');
+                breakBtn.removeAttr('disabled');
+                clearInterval(countinterval);
+                return;
+            }
 
-		//start break countdown
-		startCountdown();
-		
-	}
+            if (secNum === 0) {
+                //if secNum at zero, change seconds to 59 and reduce mins by 1
+                // minutes.text(minutesVal - 1);
+                // seconds.text(59);
+            } else {
+                if (secNum <= 10) {
+                	//if secNum is 10, set left sec to zero, reduce secNum by 1
+                    // seconds.text("0" + (secNum- 1));
+                } else {
+                	//reduce secNum by 1
+                    //seconds.text(secNum - 1);
+                }
+            }
 
-})
 
+        }, 1000);
+
+    }
+
+    function breakStart() {
+        //start break from 5:00, unless it is the third break
+        _break.attr('disabled', true);
+        _break.addClass('disabled');
+
+        //start break countdown
+        startCountdown();
+
+    }
+
+});
