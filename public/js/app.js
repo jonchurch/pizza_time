bfc3a652fb326c49dc3e183a9fe1ed8004861db8
Jon Church <me@jonchurch.com>
2016-06-22 18:19:23 -0400

    start disables on click

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	var _start = $('#start');
 	var _break = $('#break');
-	var _min-left = $('#minutes-left');
-	var _min-right = $('#minutes-right');
+	var _minLeft = $('#minutes-left');
+	var _minRight = $('#minutes-right');
+	var _secLeft = $('#seconds-left');
+	var _secRight = $('#seconds-right');
 	var breakCount= 0;
 
 	_start.on('click', startCountdown);
@@ -13,6 +15,8 @@ $(document).ready(function(){
 		//start it from 25:00
 		_start.attr('disabled', true);
 		_start.addClass('disabled');
+		//I need to associate a number with these id's somehow..
+		//_.removeClass('em-' + )
 
 
 	}
@@ -21,7 +25,11 @@ $(document).ready(function(){
 		//start break from 5:00, unless it is the third break
 		_break.attr('disabled', true);
 		_break.addClass('disabled');
+
+		//start break countdown
+		startCountdown();
 		
 	}
 
 })
+
