90ac4dfae68ed664693c503722c732fed46d00af
Jon Church <me@jonchurch.com>
2016-06-22 19:50:14 -0400

    found digit to string function on SA

--- public/js/app.js
+++ public/js/app.js
@@ -44,9 +44,8 @@ $(document).ready(function() {
                     secNum -= 1;
                 }
             }
-            emojiTime(minNum, secNum) {
+            emojiTime(minNum, secNum);
 
-            }
 
         }, 1000);
 
@@ -61,5 +60,47 @@ $(document).ready(function() {
         startCountdown();
 
     }
+
+//From SA Author: naomik http://stackoverflow.com/questions/14766951/convert-digits-into-words-with-javascript
+var toWords = function toWords(n) {
+  if (n == 0) return 'zero';
+  var a = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  var b = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  var g = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion'];
+  var grp = function grp(n) {
+    return ('000' + n).substr(-3);
+  };
+  var rem = function rem(n) {
+    return n.substr(0, n.length - 3);
+  };
+  var fmt = function fmt(_ref) {
+    var h = _ref[0];
+    var t = _ref[1];
+    var o = _ref[2];
+
+    return [Number(h) === 0 ? '' : a[h] + ' hundred ', Number(o) === 0 ? b[t] : b[t] && b[t] + '-' || '', a[t + o] || a[o]].join('');
+  };
+  var cons = function cons(xs) {
+    return function (x) {
+      return function (g) {
+        return x ? [x, g && ' ' + g || '', ' ', xs].join('') : xs;
+      };
+    };
+  };
+  var iter = function iter(str) {
+    return function (i) {
+      return function (x) {
+        return function (r) {
+          if (x === '000' && r.length === 0) return str;
+          return iter(cons(str)(fmt(x))(g[i]))(i + 1)(grp(r))(rem(r));
+        };
+      };
+    };
+  };
+  return iter('')(0)(grp(String(n)))(rem(String(n)));
+};
+
+console.log(toWords(25));
+
 
 });
